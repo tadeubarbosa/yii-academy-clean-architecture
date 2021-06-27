9b4d9ecc2d677a83b05bc861e1312439e8733542
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-06-27 10:33:57 -0300

    feat: adiciona displary erros do php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@ use App\Domain\ValueObjects\Email;
 use App\Infra\Adapters\Html2PdfAdapter;
 use App\Infra\Adapters\LocalStorageAdapter;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
