e43420975cd4a02d6462f713b22a574a687455a8
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-06-27 11:04:16 -0300

    fix: corrige estrutura da tabela registration

--- registrations.sql
+++ registrations.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `registrations` (
   `id` int NOT NULL,
+  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email` varchar(150) NOT NULL,
-  `birthdate` date NOT NULL,
-  `reigstration_number` int NOT NULL,
-  `registration_at` timestamp NOT NULL
+  `birth_date` date NOT NULL,
+  `cpf` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 ALTER TABLE `registrations`
@@ -11,5 +12,5 @@ ALTER TABLE `registrations`
   ADD UNIQUE KEY `email` (`email`);
 
 ALTER TABLE `registrations`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
