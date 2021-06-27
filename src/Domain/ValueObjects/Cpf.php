042f96c2c303c88e0da5990eb4fc3722f29ba60a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-06-27 10:28:42 -0300

    fix: corrige implementacao de cpf

--- src/Domain/ValueObjects/Cpf.php
+++ src/Domain/ValueObjects/Cpf.php
@@ -8,29 +8,29 @@ class Cpf
 
     public function __construct(string $cpf)
     {
-        if ($this->checkCpfIsInvalid()) {
+        if ($this->checkCpfIsInvalid($cpf)) {
             throw new \DomainException('CPF is not valid!');
         }
         $this->cpf = $cpf;
     }
 
-    private function checkCpfIsInvalid(): bool
+    private function checkCpfIsInvalid(string $cpf): bool
     {
-        $this->cpf = preg_replace('/\D+/', '', $this->cpf);
-        if (strlen($this->cpf) !== 11) {
+        $cpf = preg_replace('/\D+/', '', $cpf);
+        if (strlen($cpf) !== 11) {
             return false;
         }
         // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
-        if (preg_match('/(\d)\1{10}/', $this->cpf)) {
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
             return false;
         }
         // Faz o calculo para validar o CPF
         for ($t = 9; $t < 11; $t++) {
             for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $this->cpf[$c] * (($t + 1) - $c);
+                $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($this->cpf[$c] !== $d) {
+            if ($cpf[$c] !== $d) {
                 return false;
             }
         }
